02824321a61b40efe92c52c5c426248173e58c7a
Chrispine-Kanai <Chripinepeter@gmail.com>
2020-04-10 10:39:18 +0300

    1st

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,70 @@
+# def estimator(data):
+#   return data
+
+data = {
+"region": {
+"name": "Africa",
+"avgAge": 19.7,
+"avgDailyIncomeInUSD": 5,
+"avgDailyIncomePopulation": 0.71
+},
+"periodType": "days",
+"timeToElapse": 38,
+"reportedCases": 2747,
+"population": 66622705,
+"totalHospitalBeds": 1380614
+}
+
+def impactInfectionsByRequestTime():
+  currentlyInfected = int(data.get("reportedCases") * 10)
+  if data.get("periodType") == "weeks":
+    factor = int((7 * data.get("timeToElapse") // 3))
+    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+    return infectionsByRequestedTime 
+
+  if data.get("periodType") == "months":
+    factor = int((30 * data.get("timeToElapse") // 3))
+    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+    return infectionsByRequestedTime 
+
+  if data.get("periodType") == "days":
+    factor = int(data.get("timeToElapse") // 3)
+    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+    return infectionsByRequestedTime 
+
+def severeImpactInfectionsByRequestTime():
+  currentlyInfected = int(data.get("reportedCases") * 50)
+
+  if data.get("periodType") == "weeks":
+    factor = int((7 * data.get("timeToElapse") // 3))
+    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+    return infectionsByRequestedTime 
+
+  if data.get("periodType") == "months":
+    factor = int((30 * data.get("timeToElapse") // 3))
+    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+    return infectionsByRequestedTime
+  if data.get("periodType") == "days":
+    factor = int(data.get("timeToElapse") // 3)
+    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+    return infectionsByRequestedTime 
+
 def estimator(data):
-  return data
+  impact = {
+  "currentlyInfected": int(data.get("reportedCases") * 10),
+  "infectionsByRequestTime": impactInfectionsByRequestTime()
+  }
+  severeImpact = {
+  "currentlyInfected": int(data.get("reportedCases") * 50),
+  "infectionsByRequestTime": severeImpactInfectionsByRequestTime()
+  }
+
+  output = {
+  'data': data,
+  'impact': impact, 
+  'severeImpact': severeImpact
+  }
+  return output
+
+print(estimator(data))
+
