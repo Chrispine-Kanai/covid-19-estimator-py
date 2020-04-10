4d1351b17e69d11a51f6bae10bd95970dc37f0bd
Chrispine-Kanai <Chripinepeter@gmail.com>
2020-04-10 15:00:15 +0300

    3rd

--- src/estimator.py
+++ src/estimator.py
@@ -3,10 +3,10 @@
 
 data = {
   "region": {
-  "name": "Africa",
-  "avgAge": 19.7,
-  "avgDailyIncomeInUSD": 5,
-  "avgDailyIncomePopulation": 0.71
+    "name": "Africa",
+    "avgAge": 19.7,
+    "avgDailyIncomeInUSD": 5,
+    "avgDailyIncomePopulation": 0.71
   },
   "periodType": "days",
   "timeToElapse": 38,
@@ -15,54 +15,54 @@ data = {
   "totalHospitalBeds": 1380614
 }
 
+
 def impactInfectionsByRequestTime():
-  currentlyInfected = int(data.get("reportedCases") * 10)
-  if data.get("periodType") == "weeks":
-    factor = int((7 * data.get("timeToElapse") // 3))
-    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
-    return infectionsByRequestedTime 
+    currentlyInfected = int(data.get("reportedCases") * 10)
+    if data.get("periodType") == "weeks":
+      factor = int((7 * data.get("timeToElapse") // 3))
+      infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+      return infectionsByRequestedTime 
 
-  if data.get("periodType") == "months":
-    factor = int((30 * data.get("timeToElapse") // 3))
-    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
-    return infectionsByRequestedTime 
+    if data.get("periodType") == "months":
+      factor = int((30 * data.get("timeToElapse") // 3))
+      infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+      return infectionsByRequestedTime 
 
-  if data.get("periodType") == "days":
-    factor = int(data.get("timeToElapse") // 3)
-    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
-    return infectionsByRequestedTime 
+    if data.get("periodType") == "days":
+      factor = int(data.get("timeToElapse") // 3)
+      infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+      return infectionsByRequestedTime 
 
 def severeImpactInfectionsByRequestTime():
-  currentlyInfected = int(data.get("reportedCases") * 50)
+    currentlyInfected = int(data.get("reportedCases") * 50)
 
-  if data.get("periodType") == "weeks":
-    factor = int((7 * data.get("timeToElapse") // 3))
-    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
-    return infectionsByRequestedTime 
+    if data.get("periodType") == "weeks":
+      factor = int((7 * data.get("timeToElapse") // 3))
+      infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+      return infectionsByRequestedTime 
 
-  if data.get("periodType") == "months":
-    factor = int((30 * data.get("timeToElapse") // 3))
-    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
-    return infectionsByRequestedTime
-  if data.get("periodType") == "days":
-    factor = int(data.get("timeToElapse") // 3)
-    infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
-    return infectionsByRequestedTime 
+    if data.get("periodType") == "months":
+      factor = int((30 * data.get("timeToElapse") // 3))
+      infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+      return infectionsByRequestedTime
+    if data.get("periodType") == "days":
+      factor = int(data.get("timeToElapse") // 3)
+      infectionsByRequestedTime = int(currentlyInfected * (2 ** factor))
+      return infectionsByRequestedTime 
 
-def estimator(data):
-  _data = data
+def estimator(**data):
+    _data = data
 
-  return{
-    'data': _data,
+    return{
+      'data': _data,
 
-    'impact': {
-    "currentlyInfected": _data.get("reportedCases") * 10,
-    "infectionsByRequestedTime": impactInfectionsByRequestTime()
-    },
+      'impact': {
+      "currentlyInfected": _data.get("reportedCases") * 10,
+      "infectionsByRequestedTime": impactInfectionsByRequestTime()
+      },
 
-    'severeImpact': {
-    "currentlyInfected": data.get("reportedCases") * 50,
-    "infectionsByRequestedTime": severeImpactInfectionsByRequestTime()
-    }
-  }
-  
+      'severeImpact': {
+      "currentlyInfected": data.get("reportedCases") * 50,
+      "infectionsByRequestedTime": severeImpactInfectionsByRequestTime()
+      }
+}
