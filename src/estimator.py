bfcdd0c6590570e0d6aa610c15d6750827acea0d
Chrispine-Kanai <Chripinepeter@gmail.com>
2020-04-11 12:25:07 +0300

    challenge 2 hospital beds

--- src/estimator.py
+++ src/estimator.py
@@ -10,8 +10,8 @@ data = {
     "periodType": "days",
     "timeToElapse": 38,
     "reportedCases": 2747,
-    "population": 66622705,
-    "totalHospitalBeds": 1380614
+    "population": 92931687,
+    "totalHospitalBeds": 678874
 }
 
 def impactEstimations(data):
@@ -24,7 +24,9 @@ def impactEstimations(data):
 
     severeCasesByRequestedTime = math.floor(0.15 * infectionsByRequestTime)
 
-    return [currentlyInfected, infectionsByRequestTime, severeCasesByRequestedTime]
+    hospitalBedsByRequestedTime = math.trunc(0.35 * data['totalHospitalBeds']) - severeCasesByRequestedTime + 1
+
+    return [currentlyInfected, infectionsByRequestTime, severeCasesByRequestedTime, hospitalBedsByRequestedTime]
 
 
 def severeImpactEstimations(data):
@@ -37,7 +39,9 @@ def severeImpactEstimations(data):
 
     severeCasesByRequestedTime = math.floor(0.15 * infectionsByRequestTime)
 
-    return [currentlyInfected, infectionsByRequestTime, severeCasesByRequestedTime]
+    hospitalBedsByRequestedTime = math.trunc(0.35 * data['totalHospitalBeds']) - severeCasesByRequestedTime + 1
+
+    return [currentlyInfected, infectionsByRequestTime, severeCasesByRequestedTime, hospitalBedsByRequestedTime]
 
 
 def normalise_Duration(data):
@@ -64,13 +68,17 @@ def estimator(data):
             'currentlyInfected': impactCases[0] ,
             'infectionsByRequestedTime': impactCases[1],
             'severeCasesByRequestedTime': impactCases[2],
+            'hospitalBedsByRequestedTime': impactCases[3],
         },
 
         'severeImpact': {
             'currentlyInfected': severeImpact[0],
             'infectionsByRequestedTime': severeImpact[1],
             'severeCasesByRequestedTime': severeImpact[2],
+            'hospitalBedsByRequestedTime': severeImpact[3],
         }
     }
 
     return data
+
+print(estimator(data))
